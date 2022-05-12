5ebda12c830574cb8935eb3aa77abee42c0a6509
Daniel Roberto Pereira <daniel.roberto@surf.com.br>
2022-05-12 20:12:54 -0300

    add rotas de PUT, GET por ID e DELETE

--- API REST e MONGODB/src/app.js
+++ API REST e MONGODB/src/app.js
@@ -14,6 +14,11 @@ app.get('/', (req, res)=>{
     res.status(200).send('curso de node')
 })
 
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id)
+    res.json(livros[index])
+})
+
 app.get('/livros', (req, res)=>{
     res.status(200).send(livros)
 })
@@ -22,5 +27,21 @@ app.post('/livros', (req, res)=>{
     livros.push(req.body),
     res.status(201).send("Livro cadastrado com sucesso")
 })
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id)
+    livros[index].Titulo = req.body.titulo
+    res.json(livros)
+})
+
+app.delete('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id)
+    livros.splice(index, 1)
+    res.send(`Livro ${req.params.id} removido com sucesso`)
+})
+
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id)
+}
 
 export default app
